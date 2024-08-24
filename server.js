f6ff58d2dbf84668ba17f1688eb52c34a1e1ff39
sangharsh <sangharshdev69@gmail.com>
2024-08-25 04:08:29 +0530

    commit

--- server.js
+++ server.js
@@ -4,12 +4,67 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>hello World</h1>");
-})
+// connecting to mongodb server
+mongoose
+  .connect("mongodb://127.0.0.1:27017/ricoz")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Couldn't connect to MongoDB: " + err.message));
 
+// mongoose schema
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  userEmail: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model("userdata", userSchema);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+// get api to fetch all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({
+      message: "Server error while fetching users",
+      error: err.message,
+    });
+  }
+});
+
+// post api to add new user
+app.post("/add", async (req, res) => {
+  const { userName, userEmail } = req.body;
+  try {
+    const userExist = await userModel.findOne({ userEmail: userEmail });
+    if (userExist) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new userModel({ userName, userEmail });
+    const savedUser = await newUser.save();
+
+    res.status(200).json({ user: savedUser, status: "New user added successfully!!" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error while adding user", error: err.message });
+  }
+});
+
+// server
 app.listen(port, () => {
-    console.log("listening on port " + port)
-} )
+  console.log("Listening on port " + port);
+});
